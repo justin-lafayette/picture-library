2fb3a55bb327bedde486d5f8b8a44a98064d4080
aparnans <aparna_ns@yahoo.com>
2019-10-28 22:37:32 -0400

    Added find user and create user queries

--- client/controller/userController.js
+++ client/controller/userController.js
@@ -18,13 +18,14 @@ module.exports = {
         res.json(email);
       });
   }
-//,
-//   create: function(req, res) {
-//     db.User
-//       .create(req.body)
-//       .then(dbModel => res.json(dbModel))
-//       .catch(err => res.status(422).json(err));
-//   },
+,
+  create: function(req, res) {
+    db.User.create(req.body)
+      .then(function(user){
+        res.json(user);
+      });
+  }
+  //,
 //   update: function(req, res) {
 //     db.User
 //       .findOneAndUpdate({ _id: req.params.id }, req.body)
