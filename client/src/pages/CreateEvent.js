cba0cd98b27b4791f46d0e7c41f0e02c98fac273
Justin Barfield <justin.kyle.barfield@gmail.com>
2019-11-02 23:18:26 -0400

    function and export name changed to the correct name.

--- client/src/pages/CreateEvent.js
+++ client/src/pages/CreateEvent.js
@@ -8,7 +8,7 @@ import Jumbotron from '../components/Jumbotron';
 import Api from '../utils/Api';
 
 
-class Home extends Component {
+class CreateEvent extends Component {
 
     state = {
         
@@ -113,4 +113,4 @@ class Home extends Component {
     }
 };
 
-export default Home;
+export default CreateEvent;
