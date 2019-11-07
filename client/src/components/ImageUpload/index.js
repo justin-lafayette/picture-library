0aa559dab2570188018cecf48e630fe60d94c9d0
Tina Azad <mktazad@gmail.com>
2019-11-07 13:35:13 -0500

    image uploded

--- client/src/components/ImageUpload/index.js
+++ client/src/components/ImageUpload/index.js
@@ -2,24 +2,64 @@ import React, { Component } from "react";
 import Dropzone from "react-dropzone";
 // import {Button} from 'react-bootstrap'
 
-const imageMaxSize = 10;
+const imageMaxSize = 100000000;
+const acceptedFiileTypes = 'image/x-png, image/png, image/jpg, image/jpeg, image/gif'
+const acceptedFiileTypesArray = acceptedFiileTypes.split(",").map((item)=>{return item.trim()})
 class ImageUpload extends Component {
-  handleOnDrop = (files, rejectedFiles) => {
-    console.log(files);
-    console.log("rejected files are", rejectedFiles)
-    if (rejectedFiles && rejectedFiles.length> 0){
-        const currentRejectFile = rejectedFiles [0]
-        const currentRejectFileType = currentRejectFile.type
-        const currentRejectFileSize = currentRejectFile.size
-        if (currentRejectFileSize > imageMaxSize) {
-            alert ("This file is too big")
-        }
-
+  constructor(props){
+    super(props)
+    this.state = {
+      imgSrc: null
     }
-  };
+  }
+  verifyFile = (files) =>{
+    if (files && files.length > 0) {
+      const currentFile = files[0];
+      const currentFileType = currentFile.type;
+      const currentFileSize = currentFile.size;
+      if (currentFileSize > imageMaxSize) {
+        alert("This file is not allowed. " + currentFileSize + " bytes is too large")
+        return false
+      }
+      if (!acceptedFiileTypesArray.includes(currentFileType)){
+        alert("This file is not allowed. Only images are allowed.")
+        return false
+      }
+      return true
+  }
+}
+  handleOnDrop = (files, rejectedFiles) => {
+    if (rejectedFiles && rejectedFiles.length > 0) {
+      console.log(rejectedFiles)
+      this.verifyFile(rejectedFiles)
+    }
+     
+    if (files && files.length > 0) {
+      const isVerified = this.verifyFile(files)
+      if (isVerified){
+        // imageBase64Data
+        const currentFile = files[0]
+        const myFileReader = new FileReader()
+        myFileReader.addEventListener("load", () => {
+          console.log (myFileReader.result)
+          this.setState({
+            imgSrc: myFileReader.result
+          })
+        }, false)
+        myFileReader.readAsDataURL (currentFile)
+      }
+    }
+  }
   render() {
+    const {imgSrc} = this.state
     return (
       <div>
+        <h1>Image Upload</h1>
+        {imgSrc !== null ?
+        <div> 
+          {imgSrc}
+        <img src ={imgSrc} alt = 'Preview of Uploaded image' /> 
+        </div> : ''}
         <form>
           {/* <div class="form-row align-items-center">
        <div class="col-auto my-1">
@@ -36,7 +76,7 @@ class ImageUpload extends Component {
           <Dropzone
             onDrop={this.handleOnDrop}
             multiple={false}
-            accept="image/png"
+            accept={acceptedFiileTypes}
             maxSize={imageMaxSize}
             minSize={0}
           >
@@ -45,13 +85,15 @@ class ImageUpload extends Component {
                 <div {...getRootProps()}>
                   <input {...getInputProps()} />
                   <button>
-                    Drag 'n' drop some files here, or click to select files
+                    Drag 'n' drop image here, or click to select files
                   </button>
                 </div>
               </section>
             )}
           </Dropzone>
-          <button type="submit" className="btn btn-primary mb-2">Upload</button>
+          {/* <button type="submit" className="btn btn-primary mb-2">
+            Upload
+          </button> */}
         </form>
       </div>
     );
