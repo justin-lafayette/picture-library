57b55cb8b2d20fdddce2884180b0a67a18b643a6
Abigail Maldonado <doseofabby@gmail.com>
2019-11-07 11:01:53 -0500

    qrcode

--- client/src/components/QRcode/index.js
+++ client/src/components/QRcode/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import QRCode from 'react-qr-code';
-
-ReactDOM.render(
-  <QRCode value="Event" />,
-  document.getElementById('Container')
+var React = require('react');
+var QRCode = require('qrcode.react');
+ 
+React.render(
+  <QRCode value="http://facebook.github.io/react/" />,
+  mountNode
 );
+
+
+// import ReactDOM from 'react-dom';
+// import QRCode from 'react-qr-code';
+
+// ReactDOM.render(
+//   <QRCode value="Event" />,
+//   document.getElementById('Container')
+// );
+
 // function htmlEncode (value){
 //     return $('<div/>').text(value).html();
 //   }
