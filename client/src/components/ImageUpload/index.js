0153b71922d643292c739699dc3114d18645d622
Tina Azad <mktazad@gmail.com>
2019-11-06 22:18:47 -0500

    added dropzone functionality

--- client/src/components/ImageUpload/index.js
+++ client/src/components/ImageUpload/index.js
@@ -1,15 +1,27 @@
-import React, {Component} from 'react' ;
-import Dropzone from 'react-dropzone';
+import React, { Component } from "react";
+import Dropzone from "react-dropzone";
+// import {Button} from 'react-bootstrap'
 
-
-
-
+const imageMaxSize = 10;
 class ImageUpload extends Component {
-    render (){
-        return (
-            <div>
-            <form>
-     <div class="form-row align-items-center">
+  handleOnDrop = (files, rejectedFiles) => {
+    console.log(files);
+    console.log("rejected files are", rejectedFiles)
+    if (rejectedFiles && rejectedFiles.length> 0){
+        const currentRejectFile = rejectedFiles [0]
+        const currentRejectFileType = currentRejectFile.type
+        const currentRejectFileSize = currentRejectFile.size
+        if (currentRejectFileSize > imageMaxSize) {
+            alert ("This file is too big")
+        }
+
+    }
+  };
+  render() {
+    return (
+      <div>
+        <form>
+          {/* <div class="form-row align-items-center">
        <div class="col-auto my-1">
          <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
          <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
@@ -19,23 +31,31 @@ class ImageUpload extends Component {
            <option value="3">Three</option>
          </select>
        </div>
-       </div>
-           {/* // we will need to replace the console.log with our own function that reads the file */}
-           <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}> 
-           {({getRootProps, getInputProps}) => (
-               <section>
-               <div {...getRootProps()}>
-                   <input {...getInputProps()} />
-                   <button>Drag 'n' drop some files here, or click to select files</button>
-               </div>
-               </section>
-           )}
-           </Dropzone>
-            <button type="submit" class="btn btn-primary mb-2">Upload</button>
-           </form>
-       </div>
-        )
-    }
+       </div> */}
+          {/* // we will need to replace the console.log with our own function that reads the file */}
+          <Dropzone
+            onDrop={this.handleOnDrop}
+            multiple={false}
+            accept="image/png"
+            maxSize={imageMaxSize}
+            minSize={0}
+          >
+            {({ getRootProps, getInputProps }) => (
+              <section>
+                <div {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  <button>
+                    Drag 'n' drop some files here, or click to select files
+                  </button>
+                </div>
+              </section>
+            )}
+          </Dropzone>
+          <button type="submit" className="btn btn-primary mb-2">Upload</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default ImageUpload;
