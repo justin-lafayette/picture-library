e353be421731e132abdeba5fbe9575b167189a76
Abigail Maldonado <doseofabby@gmail.com>
2019-11-09 15:14:46 -0500

    qr code

--- client/src/pages/CreateEvent.js
+++ client/src/pages/CreateEvent.js
@@ -4,13 +4,13 @@ import React, { Component } from 'react';
 import Navbar from '../components/Navbar';
 import Api from '../utils/Api';
 import { Container, Jumbotron, Form, Button } from 'react-bootstrap';
-import QRCode from 'react-qr-svg'; 
+// import QRCode from 'react-qr-svg';
 
 
 class CreateEvent extends Component {
 
     state = {
-        
+
         auth: true,
         email: "",
         eventTitle: "",
@@ -18,7 +18,7 @@ class CreateEvent extends Component {
         eventDescription: "",
         qrCodeValue: ""
     }
-    
+
     // Functions
     /* TODO: Function to show event search if sign-in is valid */
 
@@ -31,17 +31,15 @@ class CreateEvent extends Component {
             event_description: this.state.eventDescription,
             email: this.state.email
         })
-            .then( res => {
+            .then(res => {
+                console.log(res.data)
                 alert("success");
                 //res.data is going to contain the event that we just created
-                //setState of qrCodeValue to that will cause a re-render
-                //in our render() method, we need to conditionally render either....
-                //a) QrCode component, supplying it the value of this.state.qrCodeValue, or
-                //b) the form that it is already rendering
-                //..BASED on whether or not this.state.qrCodeValue has a value. See comments in render() for more info
+                //setState of qrCodeValue to res.data._id, will cause a re-render
+                this.setState({qrCodeValue: res.data.event_id }) 
             })
-            .catch( err => console.log( err ) );
-        
+            .catch(err => console.log(err));
+
     }
 
     /* Handle input change */
@@ -53,87 +51,87 @@ class CreateEvent extends Component {
         // ToDo needs to be changed to email from auth.
         this.state.email = 'codybear40@gmail.com';
     }
-    
+
 
 
     // Render Elements
     render() {
-        return(
+        return (
             <div>
-        
+
                 {/* Needs to be passed as an arrow function and the onclick event written as an arrow function in the component */}
                 <Navbar
                     auth={this.state.auth}
                 />
 
                 <Container>
-                    
+
                     <Jumbotron>
                         {/* if this.state.qrCode has a value */}
                         {/* render <QrCode value={this.state.qrCodeValue} */}
                         {/* else, render the form below. NOTE: this form needs to be put into its own component so that it can be rendered conditionally (in one line)  */}
-                        { this.state.qrCode ? <QRCode value={this.state.qrCodeValue}/> : ( 
+                        {this.state.qrCodeValue ? <div><img src={"http://api.qrserver.com/v1/create-qr-code/?data=" +this.state.qrCodeValue}></img></div> : (
 
                             <Form>
 
-                            <Form.Group controlId="event-name">
-                                <Form.Label 
-                                    label="Event Name"
+                                <Form.Group controlId="event-name">
+                                    <Form.Label
+                                        label="Event Name"
                                     >
-                                    Event Name
+                                        Event Name
                                 </Form.Label>
-                                <Form.Control 
-                                    value={this.state.eventTitle}
-                                    onChange={this.handleInputChange}
-                                    name="eventTitle"
+                                    <Form.Control
+                                        value={this.state.eventTitle}
+                                        onChange={this.handleInputChange}
+                                        name="eventTitle"
                                     />
-                            </Form.Group>
+                                </Form.Group>
 
-                            <Form.Group controlId="event-date">
-                                <Form.Label
-                                    label="Event Date"
+                                <Form.Group controlId="event-date">
+                                    <Form.Label
+                                        label="Event Date"
                                     >
-                                    Event Date
+                                        Event Date
                                 </Form.Label>
-                                <Form.Control 
-                                    value={this.state.eventDate}
-                                    onChange={this.handleInputChange}
-                                    name="eventDate"
+                                    <Form.Control
+                                        value={this.state.eventDate}
+                                        onChange={this.handleInputChange}
+                                        name="eventDate"
                                     />
-                            </Form.Group>
+                                </Form.Group>
 
-                            <Form.Group controlId="event-description">
-                                <Form.Label 
-                                    label="Event Description"
+                                <Form.Group controlId="event-description">
+                                    <Form.Label
+                                        label="Event Description"
                                     >
-                                    Event Description
+                                        Event Description
                                 </Form.Label>
-                                <Form.Control
-                                    as="textarea"
-                                    rows="3" 
-                                    value={this.state.eventDescription}
-                                    onChange={this.handleInputChange}
-                                    name="eventDescription"
+                                    <Form.Control
+                                        as="textarea"
+                                        rows="3"
+                                        value={this.state.eventDescription}
+                                        onChange={this.handleInputChange}
+                                        name="eventDescription"
                                     />
-                            </Form.Group>
+                                </Form.Group>
 
-                            <Button 
-                                // disabled={!(this.state.email && this.state.password)}
-                                onClick={this.handleFormSubmit}
-                            >
-                                Submit
+                                <Button
+                                    // disabled={!(this.state.email && this.state.password)}
+                                    onClick={this.handleFormSubmit}
+                                >
+                                    Submit
                             </Button>
 
-                        </Form>
-                    )}
+                            </Form>
+                        )}
                     </Jumbotron>
 
                 </Container>
-                
+
             </div>
-                        
+
         );
-    
+
     }
 };
 
