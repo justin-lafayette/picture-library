572ae8193d99d3b886216b5398eb7b4c2cc6f34a
Michael Adams <eaglescout40@gmail.com>
2019-11-20 13:03:10 -0500

    Updating README.md

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -16,16 +16,28 @@ class About extends Component {
     const { pageNumber, numPages } = this.state;
  
     return (
+
+          <Navbar
+            isAuth={this.state.isAuth}
+          >
+              <Button
+                // disabled={!(this.state.QrReader)}
+                onClick={this.showScanner}>
+                QRscan
+              </Button>
+                        
+          </Navbar>
+
       <div>
         <Document
-          file="./assets/limits.pdf"
+          file="../assets/limits.pdf"
           onLoadSuccess={this.onDocumentLoadSuccess}
         >
           <Page pageNumber={pageNumber} />
         </Document>
         <p>Page {pageNumber} of {numPages}</p>
       </div>
-    );
+    )
   }
 }
   export default About;
